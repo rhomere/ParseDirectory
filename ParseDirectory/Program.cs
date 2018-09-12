d0d27c16b78a804954a5456291b0eac501d27a70
Renel Homere <rhomere@gmail.com>
2018-09-12 19:23:05 -0400

    add Menu, parse input, print out Folders, Filename

--- ParseDirectory/Program.cs
+++ ParseDirectory/Program.cs
@@ -10,6 +10,34 @@ namespace ParseDirectory
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Parse Directory App\n");
+            Console.WriteLine("Ex: /fileName.pdf or /folderName1/folderName2/fileName.pdf");
+            Console.Write("Enter Directory: ");
+            var input = Console.ReadLine();
+            var inputArr = input.Split('/').ToList();
+            DoSomething(inputArr);
+            
+        }
+
+        static void DoSomething(List<string> arr)
+        {
+            var fileName = arr.LastOrDefault();
+            arr.RemoveRange(0, 1);
+
+            if(arr.Count() == 1)
+            {
+                Console.WriteLine($"\nFileName: {fileName}");
+            }
+            else
+            {
+                arr.RemoveRange(arr.Count - 1, 1);
+                for (int i = 0; i < arr.Count(); i++)
+                {
+                    Console.WriteLine($"\nFolder: {arr[i]}");
+                }
+                Console.WriteLine($"\nFileName: {fileName}");
+            }
+            
         }
     }
 }
